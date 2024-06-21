76ac57fa50f1a7472b11806729e41acc6ab29382
DanSkor <133749249+DanSkor@users.noreply.github.com>
2024-06-21 20:10:20 +0000

    Deploying to gh-pages from @ DanSkor/drink2go@06c6c67188f61d0234dbd94a48c63faa284963b5 🚀

---
+++ scripts/menu.js
@@ -0,0 +1,1 @@
+var t=document.querySelector(".main-nav"),e=document.querySelector(".burger");e.addEventListener("click",()=>{t.classList.toggle("main-nav--open"),e.classList.toggle("burger--active")});
